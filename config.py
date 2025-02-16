282e2a08e39b252784d00fc90448447b59efd670
Sahil Ansari <97865856+Sahil0976@users.noreply.github.com>
2025-02-16 16:41:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API_ID = 20421248 
 API_HASH = "90da70b17365b3709b8a0346a7749ce7" 
-BOT_TOKEN = "6925557277:AAGHGPgbMDdqafiPt3oUwVqVmiqIQXJk9zg"
+BOT_TOKEN = "0"
 MONGO_URI = "0"
 START_PIC = "https://telegra.ph/file/c39602ab8e3d5932a91d1.jpg" 
 START_MSG = "<b>Hi There...! \n\nI am a File Sequence Bot.\nI can sequence your files effortlessly.\n\nMy Owner: @Its_Sahil_Ansari</b>"
