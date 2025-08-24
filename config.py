e1e898e3b47874f2b2c93e4ff9143f1ababb3e54
haruto775 <prasadpriyanshu1769@gmail.com>
2025-08-24 19:30:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 20421248 
-API_HASH = "90da70b17365b3709b8a0346a7749ce7" 
-BOT_TOKEN = "0"
-MONGO_URI = "0"
+API_ID = 23476863 
+API_HASH = "69daa0835439c4211f34c2e9ad0acb5c" 
+BOT_TOKEN = "6809164496:AAGmTBe5dCMBFH4HG6zzON87MPKsOSzo6d4"
+MONGO_URI = "mongodb+srv://Haruto179:Haruto179@cluster179.hy2mawo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster179"
 START_PIC = "https://telegra.ph/file/c39602ab8e3d5932a91d1.jpg" 
 START_MSG = "<b>Hi There...! \n\nI am a File Sequence Bot.\nI can sequence your files effortlessly.\n\nMy Owner: @Its_Sahil_Ansari</b>"
 HELP_TXT = "<b><i>About Us..\n\n‣ Made for : Anime x Hunters\n‣ Owner : @Its_Sahil_Ansari\n‣ Worked For : @Anime_x_hunters</i></b>"
