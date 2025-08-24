36aaf6207eb05f8510f099637276362f548c9eb1
haruto775 <prasadpriyanshu1769@gmail.com>
2025-08-24 19:32:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ MONGO_URI = "mongodb+srv://Haruto179:Haruto179@cluster179.hy2mawo.mongodb.net/?r
 START_PIC = "https://telegra.ph/file/c39602ab8e3d5932a91d1.jpg" 
 START_MSG = "<b>Hi There...! \n\nI am a File Sequence Bot.\nI can sequence your files effortlessly.\n\nMy Owner: @Its_Sahil_Ansari</b>"
 HELP_TXT = "<b><i>About Us..\n\n‣ Made for : Anime x Hunters\n‣ Owner : @Its_Sahil_Ansari\n‣ Worked For : @Anime_x_hunters</i></b>"
-OWNER_ID = 5090651635
+OWNER_ID = 6865669049
